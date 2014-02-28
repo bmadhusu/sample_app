3f6be7602a5f0c3890c07e551241bae7de219d86
B. Madhusudan <madhu_lists@yahoo.com>
2014-02-28 08:31:13 -0500

    Middle of Chap 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
 
   root 'static_pages#home'
   match '/signup',    to: 'users#new',  via: 'get'
@@ -7,6 +6,8 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  resources :users
 
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "B. Madhusudan"
+    email    "bmadhusu@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
