cb162ea4d53825302bf84a387d4e3111df925442
Madhu <madhu@BOMMASAMUDRAMs-MacBook-Air.local>
2014-03-08 15:35:27 -0500

    Finish Ch 8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	#	user = User.find_by(email: params[:session][:email].downcase)
+		user = User.find_by(email: params[:email].downcase)
+		if user && user.authenticate(params[:password])
+			sign_in user
+			redirect_to user
+			# sign user in
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+		sign_out
+		redirect_to root_url
+	end
+
+end
